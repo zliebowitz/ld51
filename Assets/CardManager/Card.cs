fdccf4fdd5d8a36fdab8e7191b0f0b39141a49e2
Zach Liebowitz <zach@Zliebowitz.com>
2022-09-30 21:52:02 -0400

    try actually saving files

--- Assets/CardManager/Card.cs
+++ Assets/CardManager/Card.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    public Color color;
+
+    private static Color[] color_list = new Color[] { Color.blue, Color.cyan, Color.gray, Color.green, Color.magenta, Color.red, Color.white, Color.yellow};
+
+    public Card()
+    {
+        color = color_list[Random.Range(0, color_list.Length)];
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/CardManager/CardManager.cs
+++ Assets/CardManager/CardManager.cs
@@ -4,10 +4,67 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+    List<Card> deck = new List<Card>();
+
+    List<Card> discard = new List<Card>();
+
+    List<Card> hand = new List<Card>();
+
+    int deck_size = 20;
+
+    int initial_hand_size = 4;
+
+    int max_hand_size = 7;
+
+    void Shuffle<T>(List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int other = Random.Range(i, list.Count);
+            T temp = list[i];
+            list[i] = list[other];
+            list[other] = temp;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < deck_size; i++)
+        {
+            deck.Add(new Card());
+        }
+        // Not strictly needed at the itme of writing, but can deal with alter.
+        Shuffle<Card>(deck);
         
+    }
+
+    void NewTurn()
+    {
+        foreach (var c in hand)
+            discard.Add(c);
+        hand.Clear();
+        for (int i = 0; i < initial_hand_size; i++)
+            Draw();
+    }
+
+    void Draw()
+    {
+        if (deck.Count == 0)
+        {
+            foreach (var card in discard)
+            {
+                deck.Add(card);
+            }
+            discard.Clear();
+            Shuffle(deck);
+         }
+        Card c = deck[0];
+        hand.Add(c);
+        deck.RemoveAt(0);
+        if (hand.Count > max_hand_size)
+            throw new System.Exception("invalid hand size - exceeded max");
+
     }
 
     // Update is called once per frame
