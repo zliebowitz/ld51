8161f67056aa5fd52e771130170de9bbee892e7c
Joshua Luoni <josh.luoni@gmail.com>
2022-10-02 12:38:28 -0400

    added itiatial minion logic.

--- Assets/Minions/scrap_pusher/ScrapPusher.cs
+++ Assets/Minions/scrap_pusher/ScrapPusher.cs
@@ -4,32 +4,5 @@ using UnityEngine;
 
 public class ScrapPusher : MonoBehaviour
 {
-    public UnitStats unitStats; //populate in the inspector.
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void FixedUpdate()
-    {
-        if (TOKObjectController.GetPause())
-        {
-            return;
-        }
-
-        transform.position += Vector3.right * unitStats.speed * Time.fixedDeltaTime;
-    }
-
-
-    private void OnMouseDown()
-    {
-        GetComponent<Animator>().SetTrigger("Attack");
-    }
+   
 }
