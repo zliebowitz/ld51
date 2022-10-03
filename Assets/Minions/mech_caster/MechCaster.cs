d6164d0a65b20a2458f9ceae03f508e176b775bf
Joshua Luoni <josh.luoni@gmail.com>
2022-10-03 00:20:29 -0400

    added the gunfire to mechcaster.

--- Assets/Minions/mech_caster/MechCaster.cs
+++ Assets/Minions/mech_caster/MechCaster.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class MechCaster : MonoBehaviour
 {
+    bool wasPreviousAttack = false;
+    Animator animator;
 
-   
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        
+        bool isAttack = animator.GetCurrentAnimatorStateInfo(0).IsName("Attack");
+        if (!wasPreviousAttack && isAttack)
+        {
+            FindObjectOfType<AudioManager>().PlaySound("Gunshot");
+        }
+
+        wasPreviousAttack = animator.GetCurrentAnimatorStateInfo(0).IsName("Attack");
+    }
+
 }
